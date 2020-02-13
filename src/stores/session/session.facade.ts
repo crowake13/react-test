57ebe5c2ec329ecef87139e79745f05cf7ad497c
Nikola Vranesic <nikola.vranesic@q.agency>
2020-02-13 15:27:35 +0100

    feat(auth): add and use access token via environment variables in session facade

--- src/stores/session/session.facade.ts
+++ src/stores/session/session.facade.ts
@@ -1,5 +1,5 @@
 import { BehaviorSubject, Observable } from 'rxjs';
-import { distinctUntilChanged } from 'rxjs/operators';
+import { distinctUntilChanged, map } from 'rxjs/operators';
 
 export interface ICredentials {
   email: string;
@@ -11,6 +11,7 @@ export interface ISessionFacade {
   isAuthenticating$: Observable<boolean>;
   isCurrentlyAuthenticated: boolean;
   isAuthenticated$: Observable<boolean>;
+  accessToken: string | null;
   login(creds: ICredentials): Promise<void>;
   logout(): void;
 }
@@ -25,23 +26,34 @@ export class SessionFacade implements ISessionFacade {
     return this._isAuthenticating$.getValue();
   }
 
-  private _isAuthenticated$ = new BehaviorSubject<boolean>(false);
-  readonly isAuthenticated$ = this._isAuthenticated$
-    .asObservable()
-    .pipe(distinctUntilChanged());
+  private _accessToken$ = new BehaviorSubject<string | null>(null);
+  readonly isAuthenticated$ = this._accessToken$.asObservable().pipe(
+    map(accessToken => !!accessToken),
+    distinctUntilChanged()
+  );
 
-  get isCurrentlyAuthenticated() {
-    return this._isAuthenticated$.getValue();
+  get accessToken() {
+    return this._accessToken$.getValue();
   }
 
-  private async _login({ email, password }: ICredentials): Promise<boolean> {
+  get isCurrentlyAuthenticated() {
+    return !!this.accessToken;
+  }
+
+  private async _token({
+    email,
+    password
+  }: ICredentials): Promise<string | null> {
     await this._timeout(2500);
-    return email === 'blabla' && password === 'asdf';
+    return email === process.env.REACT_APP_VALID_EMAIL &&
+      password === process.env.REACT_APP_VALID_PASSWORD
+      ? process.env.REACT_APP_ACCESS_TOKEN ?? null
+      : null;
   }
 
   async login(creds: ICredentials): Promise<void> {
     this._isAuthenticating$.next(true);
-    this._isAuthenticated$.next(await this._login(creds));
+    this._accessToken$.next(await this._token(creds));
     this._isAuthenticating$.next(false);
   }
 
@@ -50,7 +62,7 @@ export class SessionFacade implements ISessionFacade {
   }
 
   logout(): void {
-    this._isAuthenticated$.next(false);
+    this._accessToken$.next(null);
   }
 }
 

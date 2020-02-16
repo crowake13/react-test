8f9b99beec5759654b7caf7b5c3af294a50b5443
Nikola Vranesic <nikola.vranesic@q.agency>
2020-02-16 04:13:26 +0100

    feat(withrenderlog): add counter to console.log for better debugging

--- src/components/shared/withRenderLog.tsx
+++ src/components/shared/withRenderLog.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 
+let counterHashMap = new Map<string, number>();
+
 interface IWithRenderLogProps {
   greeting: string;
 }
@@ -7,23 +9,28 @@ interface IWithRenderLogProps {
 const withRenderLog = ({ greeting }: IWithRenderLogProps) => (
   WrappedComponent: any
 ) => {
-  const WithDataFetching: React.FC<{
+  const WithRenderLog: React.FC<{
     [key: string]: any;
   }> = ({ renderLogMessage, ...rest }) => {
     useEffect(() => {
+      counterHashMap.set(
+        WrappedComponent.name,
+        (counterHashMap.get(WrappedComponent.name) ?? 0) + 1
+      );
       console.log(
-        `${renderLogMessage ?? greeting} ${WrappedComponent.name}`.trim()
+        `${counterHashMap.get(WrappedComponent.name)} ${renderLogMessage ??
+          greeting} ${WrappedComponent.name}`.trim()
       );
     });
 
     return <WrappedComponent {...rest} />;
   };
 
-  WithDataFetching.displayName = `WithDataFetching(${WrappedComponent.name})`;
+  WithRenderLog.displayName = `WithRenderLog(${WrappedComponent.name})`;
 
   return process.env.NODE_ENV === 'production'
     ? WrappedComponent
-    : WithDataFetching;
+    : WithRenderLog;
 };
 
 export default withRenderLog;

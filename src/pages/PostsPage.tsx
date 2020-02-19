ef996cf217e0bb2620ed2011c2aa3afb718ce2ef
Nikola Vranesic <nikola.vranesic@q.agency>
2020-02-19 03:19:21 +0100

    refactor(fetchall): move fetching all entities to be done from a  single place

--- src/pages/PostsPage.tsx
+++ src/pages/PostsPage.tsx
@@ -1,43 +1,22 @@
-import React, { useContext, useEffect } from 'react';
+import React from 'react';
 import PostList from '../components/posts/PostList';
 import PostsSearchTerm from '../components/posts/PostsSearchTerm';
 import FetchAllStatus from '../components/shared/FetchAllStatus';
 import withRenderLog from '../components/shared/withRenderLog';
 import * as RENDER_LOG from '../constants/render-log';
-import { CommentsContext } from '../stores/comments/comments.context';
-import { PostsContext } from '../stores/posts/posts.context';
-import { UsersContext } from '../stores/users/users.context';
 
-const PostsPage = () => {
-  const usersService = useContext(UsersContext);
-  const postsService = useContext(PostsContext);
-  const commentsService = useContext(CommentsContext);
-
-  useEffect(() => {
-    usersService.fetchAll();
-  }, [usersService]);
-
-  useEffect(() => {
-    postsService.fetchAll();
-  }, [postsService]);
-
-  useEffect(() => {
-    commentsService.fetchAll();
-  }, [commentsService]);
-
-  return (
-    <div className="mb-3">
-      <div className="d-flex justify-content-between pt-3">
-        <div className="w-50 mx-3">
-          <PostsSearchTerm />
-        </div>
-        <div className="w-50 mx-3">
-          <FetchAllStatus />
-        </div>
+const PostsPage = () => (
+  <div className="mb-3">
+    <div className="d-flex justify-content-between pt-3">
+      <div className="w-50 mx-3">
+        <PostsSearchTerm />
       </div>
-      <PostList />
+      <div className="w-50 mx-3">
+        <FetchAllStatus refreshOnInit={true} />
+      </div>
     </div>
-  );
-};
+    <PostList />
+  </div>
+);
 
 export default withRenderLog({ greeting: RENDER_LOG.GREETING })(PostsPage);

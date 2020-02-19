80aba16a64b20240c1e6ec075e4154e511124334
Nikola Vranesic <nikola.vranesic@q.agency>
2020-02-19 11:59:35 +0100

    feat(entity facade): create entity facade providers for future testing

---
+++ src/components/comments/CommentsFacadeProvider.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import * as RENDER_LOG from '../../constants/render-log';
+import { CommentsContext } from '../../stores/comments/comments.context';
+import { facade, ICommentsFacade } from '../../stores/comments/comments.facade';
+import withRenderLog from '../shared/withRenderLog';
+
+interface CommentsFacadeProviderProps extends Partial<ICommentsFacade> {
+  children: any;
+}
+
+const CommentsFacadeProvider = ({
+  children,
+  ...props
+}: CommentsFacadeProviderProps) => {
+  return (
+    <CommentsContext.Provider
+      value={{
+        errors: props.errors ?? facade.errors,
+        error$: props.error$ ?? facade.error$,
+        active$: props.active$ ?? facade.active$,
+        activate: props.activate ?? facade.activate,
+        getAll: props.getAll ?? facade.getAll,
+        getById: props.getById ?? facade.getById,
+        entitie$: props.entitie$ ?? facade.entitie$,
+        isFetchingBySlug: props.isFetchingBySlug ?? facade.isFetchingBySlug,
+        isFetching: props.isFetching ?? facade.isFetching,
+        isFetching$: props.isFetching$ ?? facade.isFetching$,
+        fetch: props.fetch ?? facade.fetch,
+        fetchAll: props.fetchAll ?? facade.fetchAll
+      }}
+    >
+      {children}
+    </CommentsContext.Provider>
+  );
+};
+
+export default withRenderLog({ greeting: RENDER_LOG.GREETING })(
+  CommentsFacadeProvider
+);
---
+++ src/components/posts/PostsFacadeProvider.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import * as RENDER_LOG from '../../constants/render-log';
+import { PostsContext } from '../../stores/posts/posts.context';
+import { facade, IPostsFacade } from '../../stores/posts/posts.facade';
+import withRenderLog from '../shared/withRenderLog';
+
+interface PostsFacadeProviderProps extends Partial<IPostsFacade> {
+  children: any;
+}
+
+const PostsFacadeProvider = ({
+  children,
+  ...props
+}: PostsFacadeProviderProps) => {
+  return (
+    <PostsContext.Provider
+      value={{
+        errors: props.errors ?? facade.errors,
+        error$: props.error$ ?? facade.error$,
+        active$: props.active$ ?? facade.active$,
+        activate: props.activate ?? facade.activate,
+        getAll: props.getAll ?? facade.getAll,
+        getById: props.getById ?? facade.getById,
+        entitie$: props.entitie$ ?? facade.entitie$,
+        isFetchingBySlug: props.isFetchingBySlug ?? facade.isFetchingBySlug,
+        isFetching: props.isFetching ?? facade.isFetching,
+        isFetching$: props.isFetching$ ?? facade.isFetching$,
+        fetch: props.fetch ?? facade.fetch,
+        fetchAll: props.fetchAll ?? facade.fetchAll,
+        fetchById: props.fetchById ?? facade.fetchById
+      }}
+    >
+      {children}
+    </PostsContext.Provider>
+  );
+};
+
+export default withRenderLog({ greeting: RENDER_LOG.GREETING })(
+  PostsFacadeProvider
+);

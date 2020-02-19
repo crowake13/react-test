133f35d7ca7af0549e88f5bf83b9dadcae9cb5fa
Nikola Vranesic <nikola.vranesic@q.agency>
2020-02-19 23:40:59 +0100

    docs(session): add comments to session facade

--- src/stores/session/session.facade.ts
+++ src/stores/session/session.facade.ts
@@ -29,6 +29,10 @@ export class SessionFacade implements ISessionFacade {
   private _accessToken$ = new BehaviorSubject<string | null>(
     localStorage.getItem('token')
   );
+  /**
+   * update accessToken in localStorage every time the accesstoken is renewed
+   * and map to boolean to get isAuthenticated flag
+   */
   readonly isAuthenticated$ = this._accessToken$.asObservable().pipe(
     tap(accessToken =>
       accessToken
@@ -47,25 +51,37 @@ export class SessionFacade implements ISessionFacade {
     return !!this.accessToken;
   }
 
+  /**
+   * return accessToken if credentials are valid
+   */
   private async _token({
     email,
     password
   }: ICredentials): Promise<string | null> {
+    // delay to simulate API call
     await ((ms: number) => new Promise(resolve => setTimeout(resolve, ms)))(
       1500
     );
+
     return email === process.env.REACT_APP_VALID_EMAIL &&
       password === process.env.REACT_APP_VALID_PASSWORD
       ? process.env.REACT_APP_ACCESS_TOKEN ?? null
       : null;
   }
 
+  /**
+   * set isAuthenticating flag to true until credentials get validated
+   */
   async login(creds: ICredentials): Promise<void> {
     this._isAuthenticating$.next(true);
     this._accessToken$.next(await this._token(creds));
     this._isAuthenticating$.next(false);
   }
 
+  /**
+   * remove accessToken from store
+   * (which removes it from localStorage)
+   */
   logout(): void {
     this._accessToken$.next(null);
   }

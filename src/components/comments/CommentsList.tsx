6a3d4160c5eedd9d1cdf488e51efb02e7cd211f0
Nikola Vranesic <nikola.vranesic@q.agency>
2020-02-20 02:51:23 +0100

    refactor(comments-list-item): create a comment list item component for better scope definition

--- src/components/comments/CommentsList.tsx
+++ src/components/comments/CommentsList.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import * as RENDER_LOG from '../../constants/render-log';
 import { Comment } from '../../stores/comments/comment.model';
 import withRenderLog from '../shared/withRenderLog';
+import { CommentListItem } from './CommentsListItem';
 
 interface CommentListProps {
   comments: Comment[];
@@ -10,18 +11,12 @@ interface CommentListProps {
 export const CommentList = ({ comments }: CommentListProps) => {
   return (
     <div className="card-body m-0">
-      {comments.map(({ id, email, name, body }, index) => (
-        <div key={id} className={'mb-2' + (!index ? '' : ' border-top')}>
-          <h6 title={email} className="card-subtitle my-2 text-muted">
-            {email}
-          </h6>
-          <h5 title={name} className="card-title">
-            {name}
-          </h5>
-          <p title={body} className="card-text">
-            {body}
-          </p>
-        </div>
+      {comments.map((comment, index) => (
+        <CommentListItem
+          key={comment.id}
+          {...comment}
+          hideTopBoarder={!index}
+        />
       ))}
     </div>
   );
